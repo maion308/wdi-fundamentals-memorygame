148970369dfd2695cb96728a3a44409c3e7358bf
maion308 <michael.ion.mi@outlook.com>
2019-08-01 15:24:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,42 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamond',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[2]) {
 	console.log("You found a match!");
 } else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 }
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId].rank + " " + cards[cardId].suit
+		+ " " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
+
 	}
 }
 
