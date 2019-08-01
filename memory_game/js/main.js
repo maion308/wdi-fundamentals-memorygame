2fb9ac70cd7df06924e964d730aff5419486373a
maion308 <michael.ion.mi@outlook.com>
2019-08-01 13:20:53 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 let cards = ["queen", "queen", "king", "king"];
-let cardOne = cards[0];
-let cardTwo = cards[2];
 let cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function checkForMatch() {
+	if(cardsInPlay[0] === cardsInPlay[2]) {
+	console.log("You found a match!");
+} else {
+	console.log("Sorry, try again.");
+}
+}
 
-if (cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you found a match!");
-	} else {
-		alert("Sorry, try again.");
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 }
 
+flipCard(0);
+flipCard(2);
