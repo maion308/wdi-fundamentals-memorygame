8effae464ce5b1dfd4238ab244c36e907f11b15c
maion308 <michael.ion.mi@outlook.com>
2019-08-01 11:05:16 -0400

    added card images

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,16 @@
-let cardOne 	= "queen";
-let cardTwo 	= "king";
-let cardThree 	= "queen";
-let cardFour 	= "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
 
